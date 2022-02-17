d30718b5207f5d63486683586fdb6fbbabe4b805
woong99 <dndry1072@naver.com>
2022-02-17 22:29:59 +0900

    strict mode

---
+++ strict mode/20-01.js
@@ -0,0 +1,5 @@
+function foo() {
+  x = 10;
+}
+foo();
+console.log(x); // 10
---
+++ strict mode/20-07.js
@@ -0,0 +1,10 @@
+(function () {
+  // non-strict mode
+  var let = 10; // 에러가 발생하지 않는다.
+
+  function foo() {
+    'use strict';
+    let = 20; // SyntaxError: Unexpected strict mode reserved word
+  }
+  foo();
+})();
