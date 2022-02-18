5236f2e30998603be6315ded89b7b41225daa972
woong99 <dndry1072@naver.com>
2022-02-18 22:48:21 +0900

    this

---
+++ this/22-21.js
@@ -0,0 +1,12 @@
+function convertArgsToArray() {
+  console.log(arguments);
+
+  // arguments 객체를 배열로 변환
+  // Array.prototype.slice를 인수 없이 호출하면 배열의 복사본을 생성한다.
+  const arr = Array.prototype.slice.call(arguments);
+  console.log(arr);
+
+  return arr;
+}
+
+convertArgsToArray(1, 2, 3); // [ 1, 2, 3 ]
